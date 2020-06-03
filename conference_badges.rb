c1d8fa4869b001e626c708b808976f87b9754b30
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-03 20:44:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+badge_maker("Arel")
+
+def batch_badge_creator(attendees)
+  attendees.collect do |names|
+    "Hello, my name is #{names}."
+  end
+end
+
+def assign_rooms(attendees)
+  new_array = []
+  attendees.each_with_index do | name, index |
+    indexplusone = index + 1
+    new_array << "Hello, #{name}! You'll be assigned to room #{indexplusone}!"
+    end
+    new_array
+end
+
+def printer(attendees)
+  newest_array = [batch_badge_creator, assign_rooms]
+end
